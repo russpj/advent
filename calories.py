831116921a52c06f5abff93479b5044cf2baf1b4
Russ Paul-Jones <russ@paul-jones.org>
2023-01-12 21:45:26 -0800

    Implement basic arguments
    Help and input file

--- calories.py
+++ calories.py
@@ -8,6 +8,29 @@ from getopt import getopt, GetoptError
 
 
 def main(arguments):
+    program_name = 'calories'
+    command_line_documentation = f'{program_name} --help --file [input file]'
+    input_file = ''
+
+    try:
+        opts, args = getopt(arguments, "hf:", 
+            ("help", "file="))
+    except GetoptError:
+        print(f'Invalid Arguments: {command_line_documentation}')
+        exit(2)
+
+    for opt, arg in opts:	
+        if opt in ('-h', '--help'):
+            print(f'usage: {command_line_documentation}')
+            exit(0)
+
+        if opt in ('-f', '--file'):
+            input_file=arg
+
+    if input_file:
+        'process input'
+
+    return
 
 
 if __name__ == '__main__':
