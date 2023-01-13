7fb5dd0a601feae42dfd4f60b1738996a9f7013f
Russ Paul-Jones <russ@paul-jones.org>
2023-01-12 21:13:08 -0800

    Initial file commit

---
+++ calories.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# calories
+'''Advent of code day 1 part 1'''
+
+from sys import stdin, stdout, stderr, argv
+from getopt import getopt, GetoptError
+
+
+def main(arguments):
+
+
+if __name__ == '__main__':
+    if len(argv[1:]) == 0:
+        command_line = input('enter command line for calories: ')
+        argv.extend(command_line.split())
+    main(argv[1:])
