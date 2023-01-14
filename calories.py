86b2507ac190a34d02a1c65667c0e323676e5071
Russ Paul-Jones <russ@paul-jones.org>
2023-01-13 23:58:35 -0800

    Implement calories generator

--- calories.py
+++ calories.py
@@ -7,10 +7,30 @@ from sys import stdin, stdout, stderr, argv
 from getopt import getopt, GetoptError
 
 
+def read_calories_for_elves(input_file):
+    sum_of_calories = 0
+
+    for line in input_file:
+        line = line.strip()
+        if len(line) == 0:
+            if sum_of_calories == 0:
+                # keep reading lines before 
+                pass
+            else:
+                yield sum_of_calories
+                sum_of_calories = 0
+        else:
+            calories = int(line)
+            sum_of_calories += calories
+    if sum_of_calories:
+        yield sum_of_calories
+    return
+
+
 def main(arguments):
     program_name = 'calories'
     command_line_documentation = f'{program_name} --help --file [input file]'
-    input_file = ''
+    input_file_name = ''
 
     try:
         opts, args = getopt(arguments, "hf:", 
@@ -25,10 +45,18 @@ def main(arguments):
             exit(0)
 
         if opt in ('-f', '--file'):
-            input_file=arg
+            input_file_name = arg
 
-    if input_file:
-        'process input'
+    if input_file_name:
+        max_calories = 0
+        with open(input_file_name, 'r') as input_file:
+            max_calories = 0
+            for calories in read_calories_for_elves(input_file):
+                if calories > max_calories:
+                    max_calories = calories
+
+        print(f'the elf with the most nutrition had {max_calories} calories.')
+
 
     return
 
