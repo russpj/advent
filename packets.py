db01e47e9d6992ce7f98318f08b2ba11108c94eb
Russ Paul-Jones <russ@paul-jones.org>
2023-01-21 19:26:15 -0800

    Add packets.py
    Just a stub for now

---
+++ packets.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+# packets
+'''Advent of code day 3'''
+
+from sys import stdin, stdout, stderr, argv
+from getopt import getopt, GetoptError
+
+app_name = 'packets'
+
+def main(arguments):
+    program_name = app_name
+    command_line_documentation = f'{program_name} --help --file [input file]'
+    input_file_name = ''
+
+    try:
+        opts, args = getopt(arguments, "hf:", 
+            ("help", "file="))
+    except GetoptError:
+        print(f'Invalid Arguments: {command_line_documentation}')
+        exit(2)
+
+    for opt, arg in opts:	
+        if opt in ('-h', '--help'):
+            print(f'usage: {command_line_documentation}')
+            exit(0)
+
+        if opt in ('-f', '--file'):
+            input_file_name = arg
+
+    if input_file_name:
+        with open(input_file_name, 'r') as input_file:
+            print(f'Opened {input_file_name} for {app_name}')
+
+    return
+
+
+if __name__ == '__main__':
+    if len(argv[1:]) == 0:
+        command_line = input(f'enter command line for {app_name}: ')
+        argv.extend(command_line.split())
+    main(argv[1:])
