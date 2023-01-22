4bef9f86ac237f231851a4994d24bc3ebd79b0fd
Russ Paul-Jones <russ@paul-jones.org>
2023-01-21 20:04:52 -0800

    Add marker_finder class and add_char method
    Finds the communication marker

--- packets.py
+++ packets.py
@@ -8,6 +8,28 @@ from getopt import getopt, GetoptError
 
 app_name = 'packets'
 
+
+class marker_finder():
+    def __init__(this):
+        this.prefix = ''
+        this.num_uniq = 0
+        return
+
+    def add_char(this, char):
+        if char == '\n':
+            return -1
+        this.prefix += char
+        for look_back in range(1, this.num_uniq+1):
+            if look_back > len(this.prefix):
+                this.num_uniq += 1
+                return this.num_uniq
+            if char == this.prefix[-(look_back+1)]:
+                this.num_uniq = look_back
+                return this.num_uniq
+        this.num_uniq += 1
+        return this.num_uniq
+
+
 def main(arguments):
     program_name = app_name
     command_line_documentation = f'{program_name} --help --file [input file]'
@@ -31,6 +53,16 @@ def main(arguments):
     if input_file_name:
         with open(input_file_name, 'r') as input_file:
             print(f'Opened {input_file_name} for {app_name}')
+            for line in input_file:
+                marker = marker_finder()
+                for char in line:
+                    num_unique = marker.add_char(char)
+                    if num_unique == 4:
+                        break
+                if num_unique == -1:
+                    print(f'No marker found in {marker.prefix}.')
+                else:
+                    print(f'Found the marker. Message starts at {len(marker.prefix)}. The prefix was {marker.prefix}.')
 
     return
 
