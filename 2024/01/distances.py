b0d7b13d159321e0909efbeb4e54d67f7ea75309
Russ Paul-Jones <russ@paul-jones.org>
2024-12-03 19:21:10 -0800

    01 part a solved

---
+++ 2024/01/distances.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+# distances
+'''Advent of code Day 01'''
+
+
+from sys import stdin, stdout, stderr, argv
+from getopt import getopt, GetoptError
+
+
+app_name = 'distances.py'
+
+
+def main(arguments):
+    program_name = app_name
+    command_line_documentation = f'{program_name} --help --afile --bfile [input file]'
+    input_file_name = ''
+    section = 'a'
+
+    try:
+        opts, args = getopt(arguments, "hab:", ("help", "afile=", "bfile="))
+    except GetoptError:
+        print(f'Invalid Arguments: {command_line_documentation}')
+        exit(2)
+
+    for opt, arg in opts:	
+        if opt in ('-h', '--help'):
+            print(f'usage: {command_line_documentation}')
+            exit(0)
+
+        if opt in ('-a', '--afile'):
+            input_file_name = arg
+            section = 'a'
+
+        if opt in ('-b', '--bfile'):
+            input_file_name = arg
+            section = 'b'
+
+    if input_file_name:
+        if section == 'a':
+            left_list = []
+            right_list = []
+            with open(input_file_name, 'r') as input_file:
+                print(f'Opened {input_file_name} for {app_name} section {section}')
+                for line in input_file:
+                    left, right = [int(x) for x in line.split()]
+                    left_list.append(left)
+                    right_list.append(right)
+            left_list = sorted(left_list)
+            right_list = sorted(right_list)
+
+            sum = 0
+            for pair in zip(left_list, right_list):
+                difference = abs(pair[0]-pair[1])
+                sum += difference
+            print(f'The total distances of the {len(left_list)} pairs is {sum}')
+
+    return
+
+
+if __name__ == '__main__':
+    if len(argv[1:]) == 0:
+        command_line = input(f'enter command line for {app_name}: ')
+        argv.extend(command_line.split())
+    main(argv[1:])
