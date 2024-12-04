acde2644efa6b869c6538146db6a79b2c26820cb
Russ Paul-Jones <russ@paul-jones.org>
2024-12-03 19:51:44 -0800

    read input file

---
+++ 2024/02/safety.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+# safety
+'''Advent of code Puzzle 02'''
+
+
+from sys import stdin, stdout, stderr, argv
+from getopt import getopt, GetoptError
+
+
+app_name = 'safety.py'
+
+
+def main(arguments):
+    program_name = app_name
+    command_line_documentation = f'{program_name} --help --section [a|b] --file [input file]'
+    input_file_name = ''
+    sections = []
+
+    try:
+        opts, args = getopt(arguments, "hs:f:", ("help", "section=", "file="))
+    except GetoptError:
+        print(f'Invalid Arguments: {command_line_documentation}')
+        exit(2)
+
+    for opt, arg in opts:	
+        if opt in ('-h', '--help'):
+            print(f'usage: {command_line_documentation}')
+            exit(0)
+
+        if opt in ('-f', '--file'):
+            input_file_name = arg
+
+        if opt in ('-s', '--section'):
+            sections.append(arg)
+
+    reports = []
+    if input_file_name:
+        with open(input_file_name, 'r') as input_file:
+            print(f'Opened {input_file_name} for {app_name}')
+            for line in input_file:
+                reports.append(line.split())
+
+    for section in sections:
+        print(f'Processing {len(reports)} reports in section {section}')
+
+    return
+
+
+if __name__ == '__main__':
+    if len(argv[1:]) == 0:
+        command_line = input(f'enter command line for {app_name}: ')
+        argv.extend(command_line.split())
+    main(argv[1:])
