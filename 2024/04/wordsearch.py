7ebe9a4e754105ca723f4f61a8c3f244f87e0dbf
Russ Paul-Jones <russ@paul-jones.org>
2024-12-04 22:55:39 -0800

    create wordsearch.py from template

---
+++ 2024/04/wordsearch.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+# wordsearch
+'''Advent of code Day 04 Word Search'''
+
+
+from sys import stdin, stdout, stderr, argv
+from getopt import getopt, GetoptError
+
+
+app_name = 'wordsearch.py'
+
+
+def main(arguments):
+    program_name = app_name
+    command_line_documentation = f'{program_name} --help --section [a|b] --file [input file]'
+    input_file_name = ''
+    sections = []
+
+    try:
+        opts, args = getopt(arguments, "hs:f:", ("help", "section=", "file="))
+    except GetoptError:
+        print(f'Invalid Arguments: {command_line_documentation}')
+        exit(2)
+
+    for opt, arg in opts:	
+        if opt in ('-h', '--help'):
+            print(f'usage: {command_line_documentation}')
+            exit(0)
+
+        if opt in ('-f', '--file'):
+            input_file_name = arg
+
+        if opt in ('-s', '--section'):
+            for section in arg:
+                sections.append(section)
+
+    if input_file_name:
+        with open(input_file_name, 'r') as input_file:
+            print(f'Opened {input_file_name} for {app_name}')
+
+    for section in sections:
+        print(f'Processing section {section}')
+
+    return
+
+
+if __name__ == '__main__':
+    if len(argv[1:]) == 0:
+        command_line = input(f'enter command line for {app_name}: ')
+        argv.extend(command_line.split())
+    main(argv[1:])
