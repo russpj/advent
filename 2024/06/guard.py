28927778dbf20857e2176879c0b84b4492472d38
Russ Paul-Jones <russ@paul-jones.org>
2024-12-07 17:55:15 -0800

    count visited positions

--- 2024/06/guard.py
+++ 2024/06/guard.py
@@ -17,7 +17,26 @@ class Lab:
         self.turn = {'v': '<', '<': '^', '^': '>', '>': 'v'}
         self.block = '#'
         self.visited = 'X'
+        self.num_rows = 0
+        self.num_cols = 0
         return
+    
+    def set_map(self, map):
+        self.map = []
+        self.num_rows = len(map)
+        if self.num_rows:
+            self.num_cols = len(map[0])
+            for row in range(self.num_rows):
+                map_row = []
+                for col in range(self.num_cols):
+                    map_row.append(map[row][col])
+                self.map.append(map_row)
+
+    def print_map(self):
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                print(self.map[row][col], end='')
+            print()
     
     def guard_positions(self):
         positions = []
@@ -29,6 +48,32 @@ class Lab:
                         position = (row, col)
                         positions.append(position)
         return positions
+    
+    def count_visited_positions(self):
+        count = 0
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                if self.map[row][col] == self.visited:
+                    count += 1
+        return count
+    
+    def move_guard(self, position):
+        row = position[0]
+        col = position[1]
+        guard = self.map[row][col]
+        if guard in self.turn:
+            self.map[row][col] = self.visited
+            next_step = self.step[guard]
+            next_row = row + next_step[0]
+            next_col = col + next_step[1]
+            if next_row < 0 or next_row >= self.num_cols:
+                return
+            if next_col < 0 or next_col >= self.num_cols:
+                return
+            if self.map[next_row][next_col] == self.block:
+                self.map[row][col] = self.turn[guard]
+            else:
+                self.map[next_row][next_col] = guard
 
 
 def main(arguments):
@@ -63,20 +108,29 @@ def main(arguments):
 
     if input_file_name:
         with open(input_file_name, 'r') as input_file:
+            map = []
             print(f'Opened {input_file_name} for {app_name}')
             for line in input_file:
-                lab.map.append(line.strip())
+                map.append(line.strip())
+        lab.set_map(map)
         if verbose:
-            for row in lab.map:
-                print(row)
+            lab.print_map()
 
 
     for section in sections:
         print(f'Processing section {section}')
         if section == 'a':
-            guard_positions = lab.guard_positions()
-            for position in guard_positions:
-                print(f"There's a guard at {position}")
+            go_again = True
+            while go_again:
+                guard_positions = lab.guard_positions()
+                if len(guard_positions) <= 0:
+                    go_again = False
+                else:
+                    for position in guard_positions:
+                        lab.move_guard(position)
+                        if verbose:
+                            lab.print_map()
+            print(f'The guards visited {lab.count_visited_positions()} positions')
 
     return
 
