d02e2d88fe49b7f2f6af8fcf400fe351dff04a2a
Russ Paul-Jones <russ@paul-jones.org>
2025-01-01 03:13:08 -0800

    add antinodes.py from template

---
+++ 2024/08/antinodes.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+# antinodes
+'''Advent of code template'''
+
+
+from sys import stdin, stdout, stderr, argv
+from getopt import getopt, GetoptError
+from time import process_time
+
+
+app_name = 'antinodes.py'
+
+
+def main(arguments):
+    program_name = app_name
+    command_line_documentation = f'{program_name} --help --section [a|b] --file [input file]'
+    input_file_name = ''
+    sections = []
+
+    try:
+        opts, args = getopt(arguments, "hs:f:", ("help", "section=", "file="))
+    except GetoptError:
+        print(f'Invalid Arguments: {command_line_documentation}')
+        exit(2)
+
+    for opt, arg in opts:	
+        if opt in ('-h', '--help'):
+            print(f'usage: {command_line_documentation}')
+            exit(0)
+
+        if opt in ('-f', '--file'):
+            input_file_name = arg
+
+        if opt in ('-s', '--section'):
+            for section in arg:
+                sections.append(section)
+
+    if input_file_name:
+        with open(input_file_name, 'r') as input_file:
+            print(f'Opened {input_file_name} for {app_name}')
+
+    time_start = process_time()
+    for section in sections:
+        print(f'Processing section {section}')
+    time_end = process_time()
+    print(f'Time taken: {time_end - time_start} seconds.')
+
+    return
+
+
+if __name__ == '__main__':
+    if len(argv[1:]) == 0:
+        command_line = input(f'enter command line for {app_name}: ')
+        argv.extend(command_line.split())
+    main(argv[1:])
