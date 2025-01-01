6c00a2c473e5a9f028594459368a1b7d95d3d587
Russ Paul-Jones <russ@paul-jones.org>
2025-01-01 15:59:45 -0800

    add verbose flag

--- 2024/advent.py
+++ 2024/advent.py
@@ -14,12 +14,13 @@ app_name = 'advent.py'
 
 def main(arguments):
     program_name = app_name
-    command_line_documentation = f'{program_name} --help --section [a|b] --file [input file]'
+    command_line_documentation = f'{program_name} --help --verbose --section [a|b] --file [input file]'
+    verbose = False
     input_file_name = ''
     sections = []
 
     try:
-        opts, args = getopt(arguments, "hs:f:", ("help", "section=", "file="))
+        opts, args = getopt(arguments, "hvs:f:", ("help", "verbose", "section=", "file="))
     except GetoptError:
         print(f'Invalid Arguments: {command_line_documentation}')
         exit(2)
@@ -28,6 +29,9 @@ def main(arguments):
         if opt in ('-h', '--help'):
             print(f'usage: {command_line_documentation}')
             exit(0)
+
+        if opt in ('-v', '--verbose'):
+            verbose = True
 
         if opt in ('-f', '--file'):
             input_file_name = arg
@@ -43,6 +47,8 @@ def main(arguments):
     time_start = process_time()
     for section in sections:
         print(f'Processing section {section}')
+    if verbose:
+        print('Debugging output goes here')
     time_end = process_time()
     print(f'Time taken: {time_end - time_start} seconds.')
 
