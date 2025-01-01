db346e086f6e88fdcdcc3ad5e05f750fe53fb7e3
Russ Paul-Jones <russ@paul-jones.org>
2025-01-01 03:46:31 -0800

    read station file

--- 2024/08/antinodes.py
+++ 2024/08/antinodes.py
@@ -7,10 +7,35 @@
 from sys import stdin, stdout, stderr, argv
 from getopt import getopt, GetoptError
 from time import process_time
+from string import ascii_letters, digits
 
 
 app_name = 'antinodes.py'
 
+class Stations:
+    def __init__(self):
+        self.locations = {}
+        self.station_names = list(ascii_letters+digits)
+
+    def read_file(self, file):
+        row = 0
+        for line in file:
+            line = line.strip()
+            for col in range(len(line)):
+                station = line[col]
+                if station in self.station_names:
+                    self.add_station(station, row, col)
+            row += 1
+        return
+
+    def add_station(self, station, row, col):
+        if station in self.locations:
+            locations = self.locations[station]
+            locations.append((row, col))
+        else:
+            self.locations[station] = [(row, col)]
+        return
+        
 
 def main(arguments):
     program_name = app_name
@@ -36,9 +61,11 @@ def main(arguments):
             for section in arg:
                 sections.append(section)
 
+    stations = Stations()
     if input_file_name:
         with open(input_file_name, 'r') as input_file:
             print(f'Opened {input_file_name} for {app_name}')
+            stations.read_file(input_file)
 
     time_start = process_time()
     for section in sections:
