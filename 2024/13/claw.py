a7eecfeaed2a83042c7bde01b0dff7930d881485
Russ Paul-Jones <russ@paul-jones.org>
2025-02-02 02:59:18 -0800

    parse goals

--- 2024/13/claw.py
+++ 2024/13/claw.py
@@ -7,9 +7,49 @@
 from sys import stdin, stdout, stderr, argv
 from getopt import getopt, GetoptError
 from time import process_time
+import re
 
 
 app_name = 'claw.py'
+
+
+class Claw:
+    def __init__(self, button_a, button_b, goal):
+        self.button_a = button_a
+        self.button_b = button_b
+        self.goal = goal
+        return
+    
+    def print(self):
+        print(f'Button A: X+{self.button_a[0]}, Y+{self.button_a[1]}')
+        print(f'Button B: X+{self.button_b[0]}, Y+{self.button_b[1]}')
+        print(f'Prize: X={self.goal[0]}, Y={self.goal[1]}')
+        return
+    
+
+def parse_claws(file):
+    claws = []
+    button_test = r'Button ([A-B]): X\+(\d+), Y\+(\d+)'
+    prize_test = r'Prize: X=(\d+), Y=(\d+)'
+    for line in file:
+        matches = re.findall(button_test, line)
+        if matches:
+            button = matches[0][0]
+            x, y = [int(val) for val in matches[0][1:]]
+            if button == 'A':
+                button_a = (x, y)
+            if button == 'B':
+                button_b = (x, y)
+        matches = re.findall(prize_test, line)
+        if matches:
+            goal = [int(val) for val in matches[0]]
+            claws.append(Claw(button_a, button_b, goal))
+    return claws
+
+
+def find_solutions(claw):
+    solutions = []
+    return solutions
 
 
 def main(arguments):
@@ -43,12 +83,20 @@ def main(arguments):
     if input_file_name:
         with open(input_file_name, 'r') as input_file:
             print(f'Opened {input_file_name} for {app_name}')
+            claws = parse_claws(input_file)
 
     time_start = process_time()
     for section in sections:
         print(f'Processing section {section}')
-    if verbose:
-        print('Debugging output goes here')
+        if section == 'a':
+            for claw in claws:
+                if verbose:
+                    print('Finding solutions for:')
+                    claw.print()
+                solutions = find_solutions(claw)
+                if verbose:
+                    print(solutions)
+
     time_end = process_time()
     print(f'Time taken: {time_end - time_start} seconds.')
 
