e2fec9dc3b8b0cd72f9f97c3961e1e477bdd3bab
Russ Paul-Jones <russ@paul-jones.org>
2025-12-01 10:20:25 -0800

    copy from 2025 template

--- 2025/01/safelock.py
+++ 2025/01/safelock.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 # advent
-'''Advent of code template'''
+'''Advent of code (2025) template'''
 
 
 from sys import stdin, stdout, stderr, argv
 from getopt import getopt, GetoptError
+from time import process_time
 
 
 app_name = 'advent.py'
@@ -13,11 +14,13 @@ app_name = 'advent.py'
 
 def main(arguments):
     program_name = app_name
-    command_line_documentation = f'{program_name} --help --file [input file]'
+    command_line_documentation = f'{program_name} --help --verbose --section [a|b] --file [input file]'
+    verbose = False
     input_file_name = ''
+    sections = []
 
     try:
-        opts, args = getopt(arguments, "hf:", ("help", "file="))
+        opts, args = getopt(arguments, "hvs:f:", ("help", "verbose", "section=", "file="))
     except GetoptError:
         print(f'Invalid Arguments: {command_line_documentation}')
         exit(2)
@@ -27,12 +30,27 @@ def main(arguments):
             print(f'usage: {command_line_documentation}')
             exit(0)
 
+        if opt in ('-v', '--verbose'):
+            verbose = True
+
         if opt in ('-f', '--file'):
             input_file_name = arg
+
+        if opt in ('-s', '--section'):
+            for section in arg:
+                sections.append(section)
 
     if input_file_name:
         with open(input_file_name, 'r') as input_file:
             print(f'Opened {input_file_name} for {app_name}')
+
+    time_start = process_time()
+    for section in sections:
+        print(f'Processing section {section}')
+    if verbose:
+        print('Debugging output goes here')
+    time_end = process_time()
+    print(f'Time taken: {time_end - time_start} seconds.')
 
     return
 
