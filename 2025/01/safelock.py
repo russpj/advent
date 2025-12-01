5370d664f4083f22d539b568c7bb5ff59f309e72
Russ Paul-Jones <russ@paul-jones.org>
2025-12-01 10:42:02 -0800

    Add Dial class
    Parse is not implemented, so tests are failing.

--- 2025/01/safelock.py
+++ 2025/01/safelock.py
@@ -12,6 +12,26 @@ from time import process_time
 app_name = 'safelock.py'
 
 
+class Dial:
+    def __init__(self, pos, limit):
+        self.pos = pos 
+        self.limit = limit
+
+    def move_dial(self, instruction):
+        direction, amount = self.parse(instruction)
+        if direction == 'L':
+            amount = -amount
+        self.pos += amount
+        self.pos = self.pos%self.limit
+
+    def parse(self, instruction):
+        return ("R", 0)
+
+
+def test(dial, expected):
+    if dial.pos != expected:
+        print (f'Expected: {expected}, Actual: {dial.pos}')
+
 def main(arguments):
     program_name = app_name
     command_line_documentation = f'{program_name} --help --verbose --section [a|b] --file [input file]'
@@ -45,10 +65,23 @@ def main(arguments):
             print(f'Opened {input_file_name} for {app_name}')
 
     time_start = process_time()
+    dial = Dial(50, 100)
+
     for section in sections:
         print(f'Processing section {section}')
     if verbose:
         print('Debugging output goes here')
+        print('Running some tests')
+        test(dial, 50)
+        dial.move_dial("R10")
+        test(dial, 60)
+        dial.move_dial("L20")
+        test(dial, 40)
+        dial.move_dial("L50")
+        test(dial, 90)
+        dial.move_dial("R10")
+        test(dial, 0)
+
     time_end = process_time()
     print(f'Time taken: {time_end - time_start} seconds.')
 
