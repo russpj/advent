b4106fc71ee5d6c04702da8cf04614d1a4296d05
Russ Paul-Jones <russ@paul-jones.org>
2025-12-01 22:15:33 -0800

    customize for day 02

--- 2025/02/idvalidation.py
+++ 2025/02/idvalidation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # advent
-'''Advent of code (2025) template'''
+'''Advent of code (2025) Day 02 - Product ID Validation'''
 
 
 from sys import stdin, stdout, stderr, argv
@@ -9,7 +9,7 @@ from getopt import getopt, GetoptError
 from time import process_time
 
 
-app_name = 'advent.py'
+app_name = 'idvalidation.py'
 
 
 def main(arguments):
