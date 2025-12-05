ab9096de34d8255c6312024355b9aed866d23b99
Russ Paul-Jones <russ@paul-jones.org>
2025-12-04 19:49:00 -0800

    rename advent.py to joltage.py

--- 2025/03/joltage.py
+++ 2025/03/joltage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-# advent
-'''Advent of code (2025) template'''
+# joltage
+'''Advent of code (2025) joltage calculator'''
 
 
 from sys import stdin, stdout, stderr, argv
@@ -9,7 +9,7 @@ from getopt import getopt, GetoptError
 from time import process_time
 
 
-app_name = 'advent.py'
+app_name = 'joltage.py'
 
 
 def main(arguments):
