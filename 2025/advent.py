bd4b87e4d57ef4cecb00cfeadca8e37a839aff1f
Russ Paul-Jones <russ@paul-jones.org>
2025-12-06 15:09:42 -0800

    rename section to part
    This maps onto the instructions in the Advent of Code better. Part 1 and Part 2.

--- 2025/advent.py
+++ 2025/advent.py
@@ -14,13 +14,13 @@ app_name = 'advent.py'
 
 def main(arguments):
     program_name = app_name
-    command_line_documentation = f'{program_name} --help --verbose --section [a|b] --file [input file]'
+    command_line_documentation = f'{program_name} --help --verbose --part [1|2] --file [input file]'
     verbose = False
     input_file_name = ''
-    sections = []
+    parts = []
 
     try:
-        opts, args = getopt(arguments, "hvs:f:", ("help", "verbose", "section=", "file="))
+        opts, args = getopt(arguments, "hvp:f:", ("help", "verbose", "part=", "file="))
     except GetoptError:
         print(f'Invalid Arguments: {command_line_documentation}')
         exit(2)
@@ -36,9 +36,9 @@ def main(arguments):
         if opt in ('-f', '--file'):
             input_file_name = arg
 
-        if opt in ('-s', '--section'):
-            for section in arg:
-                sections.append(section)
+        if opt in ('-p', '--part'):
+            for part in arg:
+                parts.append(part)
 
     if input_file_name:
         with open(input_file_name, 'r') as input_file:
@@ -46,8 +46,8 @@ def main(arguments):
                 print(f'Opened {input_file_name} for {app_name}')
 
     time_start = process_time()
-    for section in sections:
-        print(f'Processing section {section}')
+    for part in parts:
+        print(f'Processing part {part}')
     if verbose:
         print('Debugging output goes here')
     time_end = process_time()
