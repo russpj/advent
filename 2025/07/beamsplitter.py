470d396f98c6ee1ffcf146b023af7e9046f6fa29
Russ Paul-Jones <russ@paul-jones.org>
2025-12-07 03:08:57 -0800

    keep count of paths into each cell

--- 2025/07/beamsplitter.py
+++ 2025/07/beamsplitter.py
@@ -18,28 +18,22 @@ class Manifold:
         for row in input_file:
             manifold.append(row.strip('\n'))
         self.manifold = manifold
-        self.beams = self.place_beams('S')
-        self.time_lines = self.place_time_lines('S')
+        self.place_beams('S')
         self.num_splits = 0
         return
 
     def place_beams(self, start_character):
         beams = set()
+        time_line_tracker = {}
         first_row = self.manifold[0]
         start_location = first_row.find(start_character)
         while start_location != -1:
             beams.add((0,start_location))
+            time_line_tracker[(0, start_location)] = 1
             start_location = first_row[start_location+1:].find(start_character)
-        return beams
-    
-    def place_time_lines(self, start_character):
-        time_lines = set()
-        first_row = self.manifold[0]
-        start_location = first_row.find(start_character)
-        while start_location != -1:
-            time_lines.add((start_location,))
-            start_location = first_row[start_location+1:].find(start_character)
-        return time_lines
+        self.beams = beams
+        self.time_line_tracker = time_line_tracker
+        return
     
     def propogate_beam(self, row, col):
         new_beams = []
@@ -52,30 +46,38 @@ class Manifold:
             else:
                 new_beams.append((row+1, col))
         return new_beams
+    
+    def add_count_of_time_lines(self, count, location):
+        if location in self.time_line_tracker:
+            old_count = self.time_line_tracker[location]
+            self.time_line_tracker[location] = old_count + count
+        else:
+            self.time_line_tracker[location] = count
+        return
 
+    def count_time_lines(self):
+        count = 0
+        row = len(self.manifold) - 1
+        for col in range(len(self.manifold[row])):
+            location = (row, col)
+            if location in self.time_line_tracker:
+                count += self.time_line_tracker[location]
+        return count
 
     def move_beams_once(self):
         new_beams = set()
         for beam in self.beams:
             row = beam[0]
             col = beam[1]
+            count_of_time_lines = self.time_line_tracker[beam]
             next_beams = self.propogate_beam(row, col)
             if len(next_beams) > 1:
                 self.num_splits += 1
             for next_beam in next_beams:
                 new_beams.add((next_beam[0], next_beam[1]))
+                self.add_count_of_time_lines(count_of_time_lines, next_beam)
         self.beams = new_beams
 
-        print(f'Moving along the time {len(self.time_lines)} time lines of length {row}')
-        new_time_lines = set()
-        for time_line in self.time_lines:
-            row = len(time_line)-1
-            col = time_line[-1]
-            next_beams = self.propogate_beam(row, col)
-            for beam in next_beams:
-                new_time_lines.add(time_line + (beam[1],))
-        if new_time_lines:
-            self.time_lines = new_time_lines            
         return
     
 
@@ -121,7 +123,7 @@ def main(arguments):
                 if part == '1':
                     print(f'the beam was split {manifold.num_splits} times')
                 if part == '2':
-                    print(f'the beam traveled through {len(manifold.time_lines)} time lines')
+                    print(f'the beam traveled through {manifold.count_time_lines()} time lines')
 
     if verbose:
         print('Debugging output goes here')
