91cdbff1032090cf69f2adf6d6dfd87a3c56c40a
Russ Paul-Jones <russ@paul-jones.org>
2025-12-08 22:39:47 -0800

    rename app to tiles

--- 2025/09/tiles.py
+++ 2025/09/tiles.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-# advent
-'''Advent of code (2025) template'''
+# tiles
+'''Advent of code (2025) 09 floor tiles'''
 
 
 from sys import stdin, stdout, stderr, argv
@@ -9,7 +9,7 @@ from getopt import getopt, GetoptError
 from time import process_time
 
 
-app_name = 'advent.py'
+app_name = 'tiles.py'
 
 
 def main(arguments):
