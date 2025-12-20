751493ebe9135091a292ce43b4713ba217a09a33
Russ Paul-Jones <russ@paul-jones.org>
2025-12-20 12:34:59 -0800

    copy advent.py to day 10

---
+++ 2025/10/buttons.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+# advent
+'''Advent of code (2025) template'''
+
+
+from sys import stdin, stdout, stderr, argv
+from getopt import getopt, GetoptError
+from time import process_time
+
+
+app_name = 'advent.py'
+
+
+def main(arguments):
+    program_name = app_name
+    command_line_documentation = f'{program_name} --help --verbose --part [1|2] --file [input file]'
+    verbose = False
+    input_file_name = ''
+    parts = []
+
+    try:
+        opts, args = getopt(arguments, "hvp:f:", ("help", "verbose", "part=", "file="))
+    except GetoptError:
+        print(f'Invalid Arguments: {command_line_documentation}')
+        exit(2)
+
+    for opt, arg in opts:	
+        if opt in ('-h', '--help'):
+            print(f'usage: {command_line_documentation}')
+            exit(0)
+
+        if opt in ('-v', '--verbose'):
+            verbose = True
+
+        if opt in ('-f', '--file'):
+            input_file_name = arg
+
+        if opt in ('-p', '--part'):
+            for part in arg:
+                parts.append(part)
+
+    if input_file_name:
+        with open(input_file_name, 'r') as input_file:
+            if verbose:
+                print(f'Opened {input_file_name} for {app_name}')
+
+    time_start = process_time()
+    for part in parts:
+        print(f'Processing part {part}')
+    if verbose:
+        print('Debugging output goes here')
+    time_end = process_time()
+    print(f'Time taken: {time_end - time_start} seconds.')
+
+    return
+
+
+if __name__ == '__main__':
+    if len(argv[1:]) == 0:
+        command_line = input(f'enter command line for {app_name}: ')
+        argv.extend(command_line.split())
+    main(argv[1:])
