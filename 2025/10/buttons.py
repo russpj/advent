023b3752c599c464ed8b65aec8e4da775a4857b0
Russ Paul-Jones <russ@paul-jones.org>
2025-12-29 10:38:10 -0800

    remove old, dead code

--- 2025/10/buttons.py
+++ 2025/10/buttons.py
@@ -53,13 +53,6 @@ def apply_rule_lights(light_state, rule):
     return ''.join(lights)
 
 
-def apply_rule_joltage(joltage_state, rule):
-    joltages = [joltage for joltage in joltage_state]
-    for joltage_index in range(len(joltage_state)):
-        joltages[joltage_index] += rule[joltage_index]
-    return tuple(joltages)
-
-
 def click_buttons_lights(machine):
     target_lights = machine[0]
     button_rules = machine[1]
@@ -81,44 +74,6 @@ def click_buttons_lights(machine):
     return
 
 
-def joltages_too_high(joltages, target_joltages):
-    for joltage_index in range(len(joltages)):
-        if joltages[joltage_index] > target_joltages[joltage_index]:
-            return True
-    return False
-
-
-def click_buttons_joltages_old(machine, verbose):
-    target_joltages = machine[2]
-    button_rules = machine[1]
-    initial_joltages = [0]*len(target_joltages)
-    click_result = (0, initial_joltages)
-    click_results = deque()
-    click_results.append(click_result)
-    queued_targets = set()
-    if verbose:
-        last_clicks = 0
-
-    while click_results:
-        previous_clicks, joltage_state = click_results.popleft()
-        if verbose and previous_clicks > last_clicks:
-            last_clicks = previous_clicks
-            print(f'{last_clicks} clicks, {len(click_results)} queued states')
-        for rule in button_rules:
-            click_result = apply_rule_joltage(joltage_state, rule)
-            if click_result == target_joltages:
-                return previous_clicks+1
-            if not click_result in queued_targets:
-                if not joltages_too_high(click_result, target_joltages):
-                    queued_targets.add(click_result)
-                    click_results.append((previous_clicks+1, click_result))
-                else:
-                    pass
-            else:
-                pass
-    return
-
-
 def click_buttons_joltages(machine, verbose):
     rules = machine[1]
     target_joltages = machine[2]
@@ -130,55 +85,18 @@ def click_buttons_joltages(machine, verbose):
     return round
 
 
-def next_digits(digits, sum_digits):
-    digit_index = 0
-
-    0, 0, 0, 3
-    0, 0, 1, 2
-    0, 0, 2, 1
-    0, 0, 3, 0
-    0, 1, 0, 2
-    0, 1, 1, 1
-    0, 1, 2, 0
-    0, 2, 0, 1
-    0, 2, 1, 0
-    0, 3, 0, 0
-    1, 0, 0, 2
-    1, 0, 1, 1
-    1, 0, 2, 0
-    1, 1, 0, 1
-    1, 1, 1, 0
-    1, 2, 0, 0
-    2, 0, 0, 1
-    2, 0, 1, 0
-    2, 1, 0, 0
-    3, 0, 0, 0
-
-
-    return False
-
-
-def odometer(num_digits, range_digits):
-    if num_digits == 1:
-        return [range_digits]
-    for trial in range(range_digits):
-        for more_digits in odometer(num_digits-1, range_digits-trial):
-            digits = [trial] + more_digits
-            yield digits
-    return
-
-
 def reverse_rule(rule, joltages):
     reversal = [joltages[i] - rule[i] for i in range(len(rule))]
     return tuple(reversal)
 
 
 def all_zero(numbers):
-    return all([number==0 for number in numbers])
+    return all(number==0 for number in numbers)
 
 
 def any_negative(numbers):
-    return any([number < 0 for number in numbers])
+    return any(number < 0 for number in numbers)
+
 
 @cache
 def satisfy(rounds_left, current_joltages, rules):
