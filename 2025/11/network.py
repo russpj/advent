6fe662f4ba548381a35a38b03d52c2ccf0cba66c
Russ Paul-Jones <russ@paul-jones.org>
2025-12-31 13:43:10 -0800

    add short circuit parameter to walk_paths()
    we need to avoid counting the paths in some cases, when we just want the list of visited nodes

--- 2025/11/network.py
+++ 2025/11/network.py
@@ -29,12 +29,13 @@ def add_destination_score(scores, destination, incoming_score):
     pass
 
 
-def walk_paths(graph, start, end, verbose=False):
+def walk_paths(graph, start, end, count_paths, verbose=False):
     queue = deque()
     num_paths = {}
     queue.append(start)
     num_paths[start] = 1
     visited = set()
+    temp_graph = {}
     while queue:
         this_node = queue.popleft()
         if this_node in visited:
@@ -46,8 +47,10 @@ def walk_paths(graph, start, end, verbose=False):
                 if verbose:
                     print(f'the number of paths from {start} to {end} is now {num_paths[end]}')
             else:
-                if this_node in graph:
+                if count_paths and this_node in graph:
                     destinations = graph[this_node]
+                    if this_node not in temp_graph:
+                        temp_graph[this_node] = destinations
                     for destination in destinations:
                         if destination in num_paths:
                             num_paths[destination] += num_paths[this_node]
@@ -55,14 +58,14 @@ def walk_paths(graph, start, end, verbose=False):
                             queue.append(destination)
                             num_paths[destination] = num_paths[this_node]
     if end in num_paths:
-        count_paths = num_paths[end]
+        count_of_paths = num_paths[end]
     else:
-        count_paths = 0
-    return (count_paths, visited)
+        count_of_paths = 0
+    return (count_of_paths, visited)
     
 
 def count_paths(graph, start, end, verbose=False):
-    return walk_paths(graph, start, end, verbose)[0]
+    return walk_paths(graph, start, end, True, verbose)[0]
 
 
 def count_paths_waypoints(graph, start, end, waypoints, verbose):
